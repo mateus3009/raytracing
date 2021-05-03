341ae9e081fbb5ed748b51d7ac493838d1899ebf
Mateus Sales Albino <mateus3009@gmail.com>
2021-05-03 17:05:18 -0300

    todas as formulas basicas adicionadas

--- src/parser/find_parser.c
+++ src/parser/find_parser.c
@@ -6,13 +6,25 @@
 /*   By: msales-a <msales-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/01 19:55:39 by msales-a          #+#    #+#             */
-/*   Updated: 2021/05/03 16:51:46 by msales-a         ###   ########.fr       */
+/*   Updated: 2021/05/03 17:03:50 by msales-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
 t_rt_parser	*g_rt_parsers = (t_rt_parser[]){
+	{.id = "R",
+		.argc = 2,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_int,
+			parse_int}},
+	{.id = "A",
+		.argc = 2,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_double,
+			parse_color}},
 	{.id = "c",
 		.argc = 3,
 		.line_parser = parse_camera,
@@ -20,6 +32,52 @@ t_rt_parser	*g_rt_parsers = (t_rt_parser[]){
 			parse_point,
 			parse_vector,
 			parse_double}},
+	{.id = "l",
+		.argc = 3,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_point,
+			parse_double,
+			parse_color}},
+	{.id = "sp",
+		.argc = 3,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_point,
+			parse_double,
+			parse_color}},
+	{.id = "pl",
+		.argc = 3,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_point,
+			parse_vector,
+			parse_color}},
+	{.id = "sq",
+		.argc = 4,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_point,
+			parse_vector,
+			parse_double,
+			parse_color}},
+	{.id = "cy",
+		.argc = 5,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_point,
+			parse_vector,
+			parse_double,
+			parse_double,
+			parse_color}},
+	{.id = "tr",
+		.argc = 4,
+		.line_parser = NULL,
+		.arg_parsers = (t_rt_arg_parser[]){
+			parse_point,
+			parse_point,
+			parse_point,
+			parse_color}},
 	NULL};
 
 bool	find_parser(char *id, int argc, t_rt_parser *parser)
